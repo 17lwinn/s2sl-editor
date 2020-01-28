49149bc1bb40f293b66994c60298faf779d9cb5d
Glitch (auroraos) <none>
2020-01-28 00:37:58 +0000

    🏃🦁 Checkpoint ./_SERVER/packages/Calculator/app.js:9935210/1340

--- _SERVER/packages/Calculator/app.js
+++ _SERVER/packages/Calculator/app.js
@@ -40,6 +40,21 @@ document.getElementById(`${mainWindow.id}Body`).onclick = function(e) {
     }
     calculator.waitingForSecondOperand = true;
     calculator.operator = e.target.value;
+    if (calculator.displayValue === "Infinity") {
+      setTimeout(function(){mainWindow.minimize();setTimeout(function(){mainWindow.minimize();setTimeout(function(){mainWindow.minimize();setTimeout(function(){mainWindow.minimize);}, 250);}, 250);}, 250);}, 250);
+      const html = document.getElementsByTagName('html')[0];
+      const longcat_src = 'https://cdn.glitch.com/d350c9dc-f43b-434e-8886-3c62df1297b2%2Flongcat.png?v=1576790382865';
+      const longcat_element = document.createElement('img');
+      longcat_element.src = longcat_src;
+      longcat_element.onload = () => {
+        longcat_element.style = "position:fixed;bottom:-10px;left:0;"
+        html.appendChild(longcat_element);
+        html.addEventListener('mousemove', e => {
+          longcat_element.style.left = `${e.clientX}px`;
+          longcat_element.style.height = `${html.clientHeight - e.clientY}px`;
+        });
+      }
+    }
     return display.value = calculator.displayValue;
   }
   if (e.target.id.includes("Decimal")) {
