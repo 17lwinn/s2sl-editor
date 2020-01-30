5a13f12a8038ef6e833bfba12f2c3c8f0585c53b
Glitch (auroraos) <none>
2020-01-30 00:24:36 +0000

    🎰🚌 Checkpoint ./_SERVER/packages/Write/app.js:9935210/925 ./_SERVER/packages/Write/main.html:9935210/1905 ./_SERVER/packages/Write/doc.html:9935210/169

--- _SERVER/packages/Write/app.js
+++ _SERVER/packages/Write/app.js
@@ -3,24 +3,21 @@ var package = os.runningPackages[document.currentScript.id];
 var mainWindowRaw = await package.resource("main.html");
 await package.createWindow(atob(mainWindowRaw), { resizable: true });
 
-await document.getElementById(document.getElementById(`${package.name}Text`)).src = `data:text/html;base64,${btoa('<head><link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"></head><body contenteditable></body>')}`;
-
+await document.getElementById(`${package.name}Text`).onload;
 var text = await document.getElementById(`${package.name}Text`).contentDocument;
 console.log(text);
-text.designMode = "on";
 
 document.getElementById(`${package.name}Bold`).onclick = function() { text.execCommand("bold", false); }
-  document.getElementById(`${package.name}Italics`).onclick = function() { text.execCommand("italic", false); }
-  document.getElementById(`${package.name}Underline`).onclick = function() { text.execCommand("underline", false); }
-  document.getElementById(`${package.name}Strikethrough`).onclick = function() { text.execCommand("strikethrough", false); }
-  document.getElementById(`${package.name}Font`).oninput = function() { text.execCommand("fontName", false, document.getElementById(`${package.name}Font`).value); }
-  document.getElementById(`${package.name}FontSize`).oninput = function() { text.execCommand("fontSize", false, document.getElementById(`${package.name}FontSize`).value); }
-  document.getElementById(`${package.name}FormatBlock`).oninput = function() { text.execCommand("removeFormat"); text.execCommand("formatBlock", false, document.getElementById(`${package.name}FormatBlock`).value); }
-  document.getElementById(`${package.name}IndentLeft`).onclick = function() { text.execCommand("justifyLeft", false); }
-  document.getElementById(`${package.name}IndentCenter`).onclick = function() { text.execCommand("justifyCenter", false); }
-  document.getElementById(`${package.name}IndentRight`).onclick = function() { text.execCommand("justifyRight", false); }
-  document.getElementById(`${package.name}OrderedList`).onclick = function() { text.execCommand("insertOrderedList", false); }
-  document.getElementById(`${package.name}UnorderedList`).onclick = function() { text.execCommand("insertUnorderedList", false); }
-  console.log(text.designMode);
-}
+document.getElementById(`${package.name}Italics`).onclick = function() { text.execCommand("italic", false); }
+document.getElementById(`${package.name}Underline`).onclick = function() { text.execCommand("underline", false); }
+document.getElementById(`${package.name}Strikethrough`).onclick = function() { text.execCommand("strikethrough", false); }
+document.getElementById(`${package.name}Font`).oninput = function() { text.execCommand("fontName", false, document.getElementById(`${package.name}Font`).value); }
+document.getElementById(`${package.name}FontSize`).oninput = function() { text.execCommand("fontSize", false, document.getElementById(`${package.name}FontSize`).value); }
+document.getElementById(`${package.name}FormatBlock`).oninput = function() { text.execCommand("removeFormat"); text.execCommand("formatBlock", false, document.getElementById(`${package.name}FormatBlock`).value); }
+document.getElementById(`${package.name}IndentLeft`).onclick = function() { text.execCommand("justifyLeft", false); }
+document.getElementById(`${package.name}IndentCenter`).onclick = function() { text.execCommand("justifyCenter", false); }
+document.getElementById(`${package.name}IndentRight`).onclick = function() { text.execCommand("justifyRight", false); }
+document.getElementById(`${package.name}OrderedList`).onclick = function() { text.execCommand("insertOrderedList", false); }
+document.getElementById(`${package.name}UnorderedList`).onclick = function() { text.execCommand("insertUnorderedList", false); }
+console.log(text.designMode);
 })()
