c0ad61d9f2f469e215ad4d73462e19b3bd761438
Glitch (auroraos) <none>
2020-02-10 23:47:22 +0000

    🌆🛀 Checkpoint ./_SERVER/packages/Market/app.js:9935210/1978

--- _SERVER/packages/Market/app.js
+++ _SERVER/packages/Market/app.js
@@ -7,10 +7,17 @@ var loading = document.getElementById(`${package.name}loading`);
 var raw = await fetch("https://aurora-market.glitch.me/homepage");
 var homepage = await raw.json();
 
-document.getElementById(`${package.name}homepage`).style.display = null;
-homepage.forEach(package => {
-  
+await homepage.forEach(async function(app) {
+  document.getElementById(`${package.name}popular`).innerHTML += `<button id="${app.name}view" style="overflow:hidden;padding:10px;width:250px;height:95px;"> <img src="data:image/webp;base64,${app.icon}" style="height:75px;width:75px;float:left;"> <strong>${app.name}</strong><br><text style="margin-top:2px;opacity:0.6;">${app.shortDescription}</text> </button>`;
+  await document.getElementById(`${app.name}view`);
+  document.getElementById(`${app.name}view`).onclick = function() { view(app.name); };
 });
 
+document.getElementById(`${package.name}homepage`).style.display = null;
 loading.style.display = "none";
+  
+function view(appName) {
+  console.log(appName);
+}
+  
 })()
