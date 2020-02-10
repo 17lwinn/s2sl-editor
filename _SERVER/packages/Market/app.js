6b51ce9d9bd294a49047e27176eb9278cc1ebfe1
Glitch (auroraos) <none>
2020-02-10 23:58:24 +0000

    🎥🍨 Checkpoint ./_SERVER/packages/Market/app.js:9935210/243 ./_SERVER/packages/Market/main.html:9935210/14

--- _SERVER/packages/Market/app.js
+++ _SERVER/packages/Market/app.js
@@ -8,10 +8,12 @@ var raw = await fetch("https://aurora-market.glitch.me/homepage");
 var homepage = await raw.json();
 
 await homepage.forEach(async function(app) {
-  document.getElementById(`${package.name}popular`).innerHTML += `<button id="${app.name}view" style="overflow:hidden;padding:10px;width:250px;height:95px;"> <img src="data:image/webp;base64,${app.icon}" style="height:75px;width:75px;float:left;"> <strong>${app.name}</strong><br><text style="margin-top:2px;opacity:0.6;">${app.shortDescription}</text> </button>`;
+  document.getElementById(`${package.name}popular`).innerHTML += `<button id="${app.name}view" style="overflow:hidden;padding:10px;width:250px;height:95px;"> <img src="data:image/webp;base64,${app.icon}" style="height:75px;width:75px;float:left;"> <strong>${app.name}</strong><br><br><text style="opacity:0.6;">${app.shortDescription}</text> </button>`;
   await document.getElementById(`${app.name}view`);
   document.getElementById(`${app.name}view`).onclick = function() { view(app.name); };
 });
+  
+document.getElementById(`${package.name}featured`).src = `data:image/webp;base64,${homepage[0].banner}`;
 
 document.getElementById(`${package.name}homepage`).style.display = null;
 loading.style.display = "none";
