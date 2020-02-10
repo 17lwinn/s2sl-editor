4c63faa16806319c4bf9bdf2d4d7c229d95b8200
Glitch (auroraos) <none>
2020-02-10 23:00:22 +0000

    🏌🏭 Checkpoint ./_SERVER/packages/Market/app.js:9935210/58 ./_SERVER/packages/Market/main.html:9935210/1171

--- _SERVER/packages/Market/app.js
+++ _SERVER/packages/Market/app.js
@@ -4,7 +4,9 @@ var wRaw = await package.resource("main.html");
 var window = await package.createWindow(atob(wRaw), { resizable: true, startingDimensions: [500, 300] });
 var loading = document.getElementById(`${package.name}loading`);
 
-var raw = await fetch("https://aurora-market.glitch.me/applists/homepage");
+var raw = await fetch("https://aurora-market.glitch.me/homepage");
 var homepage = await raw.json();
 console.log(homepage);
+  
+loading.style.display = "none";
 })()
