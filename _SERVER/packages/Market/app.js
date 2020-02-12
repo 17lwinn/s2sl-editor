bc03270a4d96283fdd7bca5e74c1eb93a49931b6
Glitch (auroraos) <none>
2020-02-12 01:51:59 +0000

    🍰⛹ Checkpoint ./_SERVER/packages/Market/app.js:9935210/1463 ./_SERVER/packages/Market/main.html:9935210/732

--- _SERVER/packages/Market/app.js
+++ _SERVER/packages/Market/app.js
@@ -6,14 +6,13 @@ var loading = document.getElementById(`${package.name}loading`);
   
 var selectCSS = "3px solid rgb(66,144,245)";
 
-function viewHomepage() {
+async function viewHomepage() {
   loading.style.display = null;
-  
-  document.getElementById(`${package.name}ViewAll`).style.borderBottom = selectCSS;
-  document.getElementById(`${package.name}ViewInstalled`).style.borderBottom = null;
   
   var raw = await fetch("https://aurora-market.glitch.me/homepage");
   var homepage = await raw.json();
+  
+  document.getElementById(`${package.name}popular`).innerHTML = null;
   
   await homepage.forEach(async function(app) {
     document.getElementById(`${package.name}popular`).innerHTML += `<button id="${app.name}view" style="overflow:hidden;padding:10px;width:250px;height:95px;"> <img src="data:image/webp;base64,${app.icon}" style="height:75px;width:75px;float:left;"> <strong>${app.name}</strong><br><br><text style="opacity:0.6;">${app.shortDescription}</text> </button>`;
@@ -22,12 +21,21 @@ function viewHomepage() {
   });
   
   document.getElementById(`${package.name}featured`).src = `data:image/webp;base64,${homepage[0].banner}`;
+  document.getElementById(`${package.name}featuredname`).innerText = homepage[0].name;
   
   document.getElementById(`${package.name}homepage`).style.display = null;
+  document.getElementById(`${package.name}info`).style.display = "none";
+  document.getElementById(`${package.name}list`).style.display = "none";
+  
+  document.getElementById(`${package.name}ViewAll`).style.borderBottom = selectCSS;
+  document.getElementById(`${package.name}ViewInstalled`).style.borderBottom = null;
+  
   loading.style.display = "none";
 }
   
 viewHomepage();
+  
+document.getElementById(`${package.name}ViewAll`).onclick = viewHomepage;
   
 async function view(app) {
   loading.style.display = null;
@@ -38,6 +46,7 @@ async function view(app) {
   document.getElementById(`${package.name}infoname`).innerText = app.name;
   document.getElementById(`${package.name}infodescription`).innerText = app.description;
   document.getElementById(`${package.name}homepage`).style.display = "none";
+  document.getElementById(`${package.name}list`).style.display = "none";
   document.getElementById(`${package.name}info`).style.display = null;
   document.getElementById(`${package.name}ViewAll`).style.borderBottom = null;
   document.getElementById(`${package.name}ViewInstalled`).style.borderBottom = null;
