32da2a6c81e1d04da17108081534e6e6a5074e8d
Glitch (auroraos) <none>
2020-02-12 01:28:00 +0000

    🤗🔋 Checkpoint ./_SERVER/packages/Market/main.html:9935210/1084 ./_SERVER/packages/Market/app.js:9935210/1440

--- _SERVER/packages/Market/app.js
+++ _SERVER/packages/Market/app.js
@@ -10,7 +10,7 @@ var homepage = await raw.json();
 await homepage.forEach(async function(app) {
   document.getElementById(`${package.name}popular`).innerHTML += `<button id="${app.name}view" style="overflow:hidden;padding:10px;width:250px;height:95px;"> <img src="data:image/webp;base64,${app.icon}" style="height:75px;width:75px;float:left;"> <strong>${app.name}</strong><br><br><text style="opacity:0.6;">${app.shortDescription}</text> </button>`;
   await document.getElementById(`${app.name}view`);
-  document.getElementById(`${app.name}view`).onclick = function() { view(app.name); };
+  document.getElementById(`${app.name}view`).onclick = function() { view(app); };
 });
 
 document.getElementById(`${package.name}featured`).src = `data:image/webp;base64,${homepage[0].banner}`;
@@ -18,9 +18,17 @@ document.getElementById(`${package.name}featured`).src = `data:image/webp;base64
 document.getElementById(`${package.name}homepage`).style.display = null;
 loading.style.display = "none";
   
-async function view(appName) {
-  var raw = await fetch(`https://aurora-market.glitch.me/apps/${appName}`);
+async function view(app) {
+  loading.style.display = null;
+  var raw = await fetch(`https://aurora-market.glitch.me/apps/${app.name}`);
   var info = await raw.json();
+  document.getElementById(`${package.name}infobanner`).src = `https://media.discordapp.net/attachments/605344668330491925/676956851312525312/unknown.png?width=1080&height=114`;
+  document.getElementById(`${package.name}infoicon`).src = `data:image/webp;base64,${app.icon}`;
+  document.getElementById(`${package.name}infoname`).innerText = app.name;
+  document.getElementById(`${package.name}infodescription`).innerText = app.description;
+  document.getElementById(`${package.name}homepage`).style.display = "none";
+  document.getElementById(`${package.name}info`).style.display = null;
+  loading.style.display = "none";
 }
   
 })()
