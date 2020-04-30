99263b2dea47847100d76c021f56b4bccccc9204
Glitch (auroraos) <none>
2020-04-30 16:15:05 +0000

    📚🚔 Checkpoint ./default.css:9935210/4 ./PWA-service.js:9935210/1002 ./PWA/noInternet.html:9935210/686

--- PWA-service.js
+++ PWA-service.js
@@ -1,10 +1,8 @@
 self.addEventListener("install", function(event) {
   //console.log("Update " + Math.random().toString());
   event.waitUntil(
-    caches.open("v1").then((cache) => {
+    caches.open("cache").then((cache) => {
       return cache.addAll([
-        "PWA/noInternet.html",
-        "default.css",
         "logo.webp",
         "wallpaper.webp",
         "loading.webp",
@@ -23,7 +21,7 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        if (["v1"].indexOf(key) === -1) {
+        if (["cache"].indexOf(key) === -1) {
           return caches.delete(key);
         }
       }));
@@ -33,7 +31,7 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", function(event) {
   event.respondWith(caches.match(event.request).then(response => {
-    if (!response && navigator.onLine === false) return caches.match("PWA/noInternet.html");
+    if (!response && navigator.onLine === false) return new Response(`<body style="font-family:segoe ui,'open sans',sans-serif;background:black;color:white;text-align:center;"><img src="logo.webp" style="position:fixed;top:50%;left:50%;margin-top:-95px;margin-left:-75px;"><p>Server is down, or you are not connected to the internet.</p></body>`,{headers:{"Content-Type":"text/html"}});
     return response ? response : fetch(event.request);
   }));
 });
