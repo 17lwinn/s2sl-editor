00143d44bdb763c7d5f1b097e1cd98c3b82fee3e
Glitch (auroraos) <none>
2020-04-30 00:58:12 +0000

    ✂️💋 Checkpoint ./PWA/noInternet.html:9935210/1128 ./PWA-service.js:9935210/1786

--- PWA-service.js
+++ PWA-service.js
@@ -1,16 +1,24 @@
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open("v1").then((cache) => {
-      return cache.addAll(["logo.webp", "PWA/noInternet.html"]);
+      return cache.addAll(["logo.webp", "PWA/noInternet.html", "https://fonts.googleapis.com/css?family=Open+Sans", "default.css"]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        return caches.delete(key);
+      }));
     })
   );
 });
 
 self.addEventListener("fetch", function(event) {
-  console.log(navigator.onLine)
-  if (event.request.url.includes("logo.webp") && navigator.onLine == false) {
-    event.respondWith(caches.match(event.request));
-  } else if (navigator.onLine === false) {
-    event.respondWith(caches.match("PWA/noInternet.html"));
-  } else event.respondWith(fetch(event.request));
+  event.respondWith(caches.match(event.request).then(response => {
+    if (!response && navigator.onLine === false) return caches.match("PWA/noInternet.html");
+    return response || fetch(event.request);
+  }));
 });
