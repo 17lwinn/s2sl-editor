b4bf1e055f27349ebf758b54e7cc11abb8d267af
Glitch (auroraos) <none>
2020-04-30 01:34:11 +0000

    🐘🍇 Checkpoint ./PWA-service.js:9935210/452 ./min.svg:9935210/46 ./max.svg:9935210/22 ./close.svg:9935210/25 ./_SERVER/packages/Settings/main.html:9935210/2

--- PWA-service.js
+++ PWA-service.js
@@ -2,7 +2,19 @@ self.addEventListener("install", function(event) {
   console.log("update " + Math.random().toString());
   event.waitUntil(
     caches.open("v1").then((cache) => {
-      return cache.addAll(["PWA/noInternet.html", "index.html", "default.css", "logo.webp", "wallpaper.webp", "loading.webp", "jquery.js", "core.js"]);
+      return cache.addAll([
+        "PWA/noInternet.html",
+        "default.css",
+        "logo.webp",
+        "wallpaper.webp",
+        "loading.webp",
+        "jquery.js",
+        "core.js",
+        "close.svg", "max.svg", "min.svg",
+        "https://fonts.googleapis.com/css?family=Open+Sans",
+        "https://fonts.gstatic.com/s/opensans/v17/mem8YaGs126MiZpBA-UFWJ0bbck.woff2",
+        "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"
+      ]);
     })
   );
 });
@@ -22,6 +34,6 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", function(event) {
   event.respondWith(caches.match(event.request).then(response => {
     if (!response && navigator.onLine === false) return caches.match("PWA/noInternet.html");
-    return response || fetch(event.request);
+    return response ? response : fetch(event.request);
   }));
 });
