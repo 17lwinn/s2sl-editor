67dedaee8db794fcc48473ef2622c33ceb70bd57
Glitch (s2sl-code) <none>
2020-05-26 18:34:38 +0000

    🐉💃 Checkpoint ./index.html:5152800/959 ./package.json:5152800/2

--- _SERVER/packages/UI/app.js
+++
@@ -1,5 +0,0 @@
-;(async function(){
-var package = os.runningPackages[document.currentScript.id];
-var mainWindowRaw = await package.resource("main.html");
-package.createWindow(atob(mainWindowRaw), { resizable: true });
-})()
